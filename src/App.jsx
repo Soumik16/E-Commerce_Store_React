8e15013f162ba8e80da6fb1421ef23528a304d5a
Soumik Neogi <133677571+Soumik16@users.noreply.github.com>
2024-07-20 23:45:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from "./pages/home/HomePage";
 import NoPage from "./pages/noPage/NoPage";
 import ProductInfo from "./pages/productInfo/ProductInfo";
@@ -20,40 +20,38 @@ import CategoryPage from "./pages/category/CategoryPage";
 const App = () => {
   return (
     <MyState>
-      <Router>
-        <ScrollTop />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/*" element={<NoPage />} />
-          <Route path="/productinfo/:id" element={<ProductInfo />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/allproduct" element={<AllProduct />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/category/:categoryname" element={<CategoryPage />} />  {/* category Page route  */}
-          <Route path="/user-dashboard" element={
-            <ProtectedRouteForUser>
-              <UserDashboard />
-            </ProtectedRouteForUser>
-          } />
-          <Route path="/admin-dashboard" element={
-            <ProtectedRouteForAdmin>
-              <AdminDashboard />
-            </ProtectedRouteForAdmin>
-          } />
-          <Route path="/addproduct" element={
-            <ProtectedRouteForAdmin>
-              <AddProductPage />
-            </ProtectedRouteForAdmin>
-          } />
-          <Route path="/updateproduct/:id" element={
-            <ProtectedRouteForAdmin>
-              <UpdateProductPage />
-            </ProtectedRouteForAdmin>
-          } />
-        </Routes>
-        <Toaster />
-      </Router>
+      <ScrollTop />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/*" element={<NoPage />} />
+        <Route path="/productinfo/:id" element={<ProductInfo />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/allproduct" element={<AllProduct />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/category/:categoryname" element={<CategoryPage />} />  {/* category Page route  */}
+        <Route path="/user-dashboard" element={
+          <ProtectedRouteForUser>
+            <UserDashboard />
+          </ProtectedRouteForUser>
+        } />
+        <Route path="/admin-dashboard" element={
+          <ProtectedRouteForAdmin>
+            <AdminDashboard />
+          </ProtectedRouteForAdmin>
+        } />
+        <Route path="/addproduct" element={
+          <ProtectedRouteForAdmin>
+            <AddProductPage />
+          </ProtectedRouteForAdmin>
+        } />
+        <Route path="/updateproduct/:id" element={
+          <ProtectedRouteForAdmin>
+            <UpdateProductPage />
+          </ProtectedRouteForAdmin>
+        } />
+      </Routes>
+      <Toaster />
     </MyState>
   );
 }
